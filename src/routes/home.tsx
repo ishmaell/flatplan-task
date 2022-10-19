2fc5d50dfab5d68e887a2d7bb0c0bc05f2f6376e
ishmaell <isiaka.ismaila@gmail.com>
2022-10-19 13:27:32 +0100

    chore: add platform useage

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import Phone from '../assets/images/phone.png';
+import Favorite from '../assets/images/favorite.svg';
+import Bell from '../assets/images/bell.svg';
+import Star from '../assets/images/Star.svg';
+import Camera from '../assets/images/camera.svg';
+import Lock from '../assets/images/lock.svg';
+import Pin from '../assets/images/pin.svg';
 
 const Home = () => {
   return (
@@ -15,6 +21,55 @@ const Home = () => {
               dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
               amet.
             </p>
+          </div>
+        </div>
+      </div>
+      <div className="useage">
+        <div className="container">
+          <h2>Why Use Our Platform</h2>
+          <div className="useage-items">
+            <div className="useage-item">
+              <img src={Favorite} alt="Favorite" />
+              <div className="useage-item-text">
+                <p>The best application</p>
+                <p>Donec facilis tortor ut lorem ipsum dolor sit amet...</p>
+              </div>
+            </div>
+            <div className="useage-item">
+              <img src={Lock} alt="Lock" />
+              <div className="useage-item-text">
+                <p>Secure shopping</p>
+                <p>Donec facilis tortor ut lorem ipsum dolor sit amet...</p>
+              </div>
+            </div>
+            <div className="useage-item">
+              <img src={Star} alt="Star" />
+              <div className="useage-item-text">
+                <p>Rate and review</p>
+                <p>Donec facilis tortor ut lorem ipsum dolor sit amet...</p>
+              </div>
+            </div>
+            <div className="useage-item">
+              <img src={Bell} alt="Bell" />
+              <div className="useage-item-text">
+                <p>Get notifications</p>
+                <p>Donec facilis tortor ut lorem ipsum dolor sit amet...</p>
+              </div>
+            </div>
+            <div className="useage-item">
+              <img src={Camera} alt="Camera" />
+              <div className="useage-item-text">
+                <p>Send video</p>
+                <p>Donec facilis tortor ut lorem ipsum dolor sit amet...</p>
+              </div>
+            </div>
+            <div className="useage-item">
+              <img src={Pin} alt="Pin" />
+              <div className="useage-item-text">
+                <p>Fast buy and sell.</p>
+                <p>Donec facilis tortor ut lorem ipsum dolor sit amet...</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
